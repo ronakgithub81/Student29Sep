313ea46384eeda6a7d93f019efc5c35c2a6cbd38
ronak1 <ronak.sheth@sheridancollege.ca>
2021-10-06 18:50:42 -0400

    adding new class called full time student

---
+++ src/student/FullTimeStudent.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package student;
+
+/**
+ *
+ * @author Ronak
+ */
+public class FullTimeStudent extends Student{
+    private int numSemesters;
+
+    public int getNumSemesters() {
+        return numSemesters;
+    }
+
+    public void setNumSemesters(int numSemesters) {
+        this.numSemesters = numSemesters;
+    }
+
+    
+    
+    
+}
