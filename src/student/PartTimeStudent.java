eae73a2f3284a89230f30844ad9449393d549533
ronak1 <ronak.sheth@sheridancollege.ca>
2021-10-06 18:26:33 -0400

    adding new class called part time student

---
+++ src/student/PartTimeStudent.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package student;
+
+/**
+ *
+ * @author Ronak
+ */
+public class PartTimeStudent extends Student{
+    private int numOfCourses;
+
+    public int getNumOfCourses() {
+        return numOfCourses;
+    }
+
+    public void setNumOfCourses(int numOfCourses) {
+        this.numOfCourses = numOfCourses;
+    }
+  
+    
+}
